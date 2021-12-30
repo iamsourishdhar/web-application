585892c2f33beb05058b4a523718e0f47955ea82
iamsourishdhar <iamsourishdhar@gmail.com>
2021-12-30 12:12:45 +0530

    Latest Code

--- src/main/java/com/demo/project/webapplication/controllers/UserController.java
+++ src/main/java/com/demo/project/webapplication/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.demo.project.webapplication.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +20,9 @@ public class UserController {
 	
 	@Autowired
 	private UserServiceIF userServiceIF;
+	@Value("${microservices.name}")
+	private String componentName;
+	
 		
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String printHelloWorld() {
@@ -39,5 +44,10 @@ public class UserController {
 	public ResponseEntity<String> addUser(@RequestBody User user) throws Exception {
 		String str = userServiceIF.addUser(user);
 		return new ResponseEntity<String>(str, HttpStatus.CREATED);
+	}
+	
+	@GetMapping(path = "/testyaml")
+	public void displayYamlValues() {
+		System.out.println("Component Name " + componentName);
 	}
 }
